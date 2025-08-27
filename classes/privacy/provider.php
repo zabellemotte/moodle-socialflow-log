158d59b9953d9552c3d300295f00d9a6f76d7312
Isabelle Motte <38882780+zabellemotte@users.noreply.github.com>
2025-08-27 22:18:49 +0200

    Final version based on logstore_lanalytics

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -18,93 +18,29 @@
  * Data provider.
  *
  * @package logstore_socialflow
- * Fork of logstore_standard
- * @copyright  2018 Frédéric Massart
- * @author     Frédéric Massart <fred@branchup.tech>
+ * Fork of logstore_lanalytics
+ * @copyright   Lehr- und Forschungsgebiet Ingenieurhydrologie - RWTH Aachen University
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * Modified by Zabelle Motte (UCLouvain)
  */
 
 namespace logstore_socialflow\privacy;
-defined('MOODLE_INTERNAL') || die();
-
-use context;
-use core_privacy\local\metadata\collection;
-use core_privacy\local\request\contextlist;
 
 /**
- * Data provider class.
+ * Privacy Subsystem implementation for logstore_socialflow.
  *
+ * This provider declares that the plugin does not store any personal data.
+ *
+ * @package    logstore_socialflow
+ * @category   privacy
  */
-class provider implements
-    \core_privacy\local\metadata\provider,
-    \tool_log\local\privacy\logstore_provider,
-    \tool_log\local\privacy\logstore_userlist_provider {
-
-    use \tool_log\local\privacy\moodle_database_export_and_delete;
-
+class provider implements \core_privacy\local\metadata\null_provider {
     /**
-     * Returns metadata.
+     * Explain that this plugin does not store any personal data.
      *
-     * @param collection $collection The initialised collection to add items to.
-     * @return collection A listing of user data stored through this system.
+     * @return string Language string identifier.
      */
-    public static function get_metadata(collection $collection) : collection {
-        $collection->add_database_table('logstore_socialflow_log', [
-            'eventid' => 'privacy:metadata:log:eventid',
-            'userid' => 'privacy:metadata:log:userid',
-            'timecreated' => 'privacy:metadata:log:timecreated',
-        ], 'privacy:metadata:log');
-        return $collection;
-    }
-    
-        /**
-     * Add contexts that contain user information for the specified user.
-     *
-     * @param contextlist $contextlist The contextlist to add the contexts to.
-     * @param int $userid The user to find the contexts for.
-     * @return void
-     */
-    public static function add_contexts_for_userid(contextlist $contextlist, $userid) {
-        $sql = "
-            SELECT l.contextid
-              FROM {logstore_socialflow_log} l
-             WHERE l.userid = :userid
-        $contextlist->add_from_sql($sql, [
-            'userid' => $userid
-        ]);
-    }
-    
-        /**
-     * Add user IDs that contain user information for the specified context.
-     *
-     * @param \core_privacy\local\request\userlist $userlist The userlist to add the users to.
-     * @return void
-     */
-    public static function add_userids_for_context(\core_privacy\local\request\userlist $userlist) {
-        $params = ['contextid' => $userlist->get_context()->id];
-        $sql = "SELECT userid, relateduserid, realuserid
-                  FROM {logstore_socialflow_log}
-                 WHERE contextid = :contextid";
-        $userlist->add_from_sql('userid', $sql, $params);
-    }
-
-  /**
-     * Get the database object.
-     *
-     * @return array Containing moodle_database, string, or null values.
-     */
-    protected static function get_database_and_table() {
-        global $DB;
-        return [$DB, 'logstore_socialflow_log'];
-    }
-
-   /**
-     * Get the path to export the logs to.
-     *
-     * @return array
-     */
-    protected static function get_export_subcontext() {
-        return [get_string('privacy:path:logs', 'tool_log'), get_string('pluginname', 'logstore_socialflow')];
+    public static function get_reason(): string {
+        return 'privacy:metadata';
     }
 }
-
