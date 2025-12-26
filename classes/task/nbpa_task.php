b98f7ff22ed9032df84fcb2691cc7e26e2cf96f4
Isabelle Motte <38882780+zabellemotte@users.noreply.github.com>
2025-12-26 14:41:38 +0100

    Optimize the request to update nbpa informations

--- classes/task/nbpa_task.php
+++ classes/task/nbpa_task.php
@@ -62,65 +62,47 @@ class nbpa_task extends \core\task\scheduled_task {
             die("temporary nbpa table creation impossible");
         }
 
-        // Get list of all courses in log table.
-        $sql2 = "SELECT DISTINCT(courseid) FROM mdl_logstore_socialflow_log";
-        $result2 = $DB->get_records_sql($sql2);
-        if (!$result2) {
-            die("no data to treat");
-        }
         $now = time();
-        // Get number of active participants in the course.
-        // Numbrer of active participants needs a heavy computation.
-        // Verifications : active user, active enrolment method, active enrolment, student role.
-        // It is sufficient to compute it 1 time a day, so it is stored in a dedicated table.
-        // That is updated via this nbpa crontask.
-        foreach ($result2 as $row) {
-            $courseid = $row->courseid;
-            $sql3 = "
-                SELECT COUNT(DISTINCT u.id) AS nbpa
-                  FROM {user} u
-                  JOIN {user_enrolments} ue ON ue.userid = u.id
-                  JOIN {enrol} e ON e.id = ue.enrolid
-                  JOIN {role_assignments} ra ON ra.userid = u.id
-                  JOIN {context} ct
-                      ON ct.id = ra.contextid
-                     AND ct.contextlevel = :contextlevel
-                  JOIN {course} c
-                      ON c.id = ct.instanceid
-                     AND e.courseid = c.id
-                  JOIN {role} r
-                      ON r.id = ra.roleid
-                     AND r.shortname = :roleshortname
-                 WHERE e.status = :enrolstatus
-                   AND ue.status = :uestatus
-                   AND u.suspended = :suspended
-                   AND u.deleted = :deleted
-                   AND (ue.timeend = 0 OR ue.timeend > :now)
-                   AND c.id = :courseid
-            ";
-            $params3 = [
-                'contextlevel'  => CONTEXT_COURSE,
-                'roleshortname' => 'student',
-                'enrolstatus'   => 0,
-                'uestatus'      => 0,
-                'suspended'     => 0,
-                'deleted'       => 0,
-                'now'           => $now,
-                'courseid'      => $courseid,
-            ];
-            $result3 = $DB->get_record_sql($sql3, $params3);
-            if (!$result3) {
-                die("nbpa computation impossible");
-            }
-            $nbpa = $result3->nbpa;
-            $data = new \stdClass();
-            $data->courseid = $courseid;
-            $data->nbpa = $nbpa;
-            $result4 = $DB->insert_record('logstore_socialflow_nbpa_temp', $data);
-            if (!$result4) {
-                die("insert new nbpa data impossible");
-            }
-        }
+
+        $sql = "
+            INSERT INTO {logstore_socialflow_nbpa_temp} (courseid, nbpa)
+            SELECT
+                c.id AS courseid,
+                COUNT(DISTINCT u.id) AS nbpa
+            FROM {user} u
+            JOIN {user_enrolments} ue ON ue.userid = u.id
+            JOIN {enrol} e ON e.id = ue.enrolid
+            JOIN {role_assignments} ra ON ra.userid = u.id
+            JOIN {context} ct
+                ON ct.id = ra.contextid
+               AND ct.contextlevel = :contextlevel
+            JOIN {course} c
+                ON c.id = ct.instanceid
+               AND e.courseid = c.id
+            JOIN {role} r
+                ON r.id = ra.roleid
+               AND r.shortname = :roleshortname
+            JOIN {logstore_socialflow_log} l
+                ON l.courseid = c.id
+            WHERE e.status = :enrolstatus
+                AND ue.status = :uestatus
+                AND u.suspended = :suspended
+                AND u.deleted = :deleted
+                AND (ue.timeend = 0 OR ue.timeend > :now)
+            GROUP BY c.id
+        ";
+
+        $params = [
+            'contextlevel'  => CONTEXT_COURSE,
+            'roleshortname' => 'student',
+            'enrolstatus'   => 0,
+            'uestatus'      => 0,
+            'suspended'     => 0,
+            'deleted'       => 0,
+            'now'           => $now,
+        ];
+
+        $DB->execute($sql, $params);
         mtrace("Computations completed, data replacement begins ...");
 
         // Table nbpa replacement and temporary table dropping.
